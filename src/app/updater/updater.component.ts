ceb19e6e53ff8ba9e324d8d0e2b5a361814e1708
voc0der <23507129+voc0der@users.noreply.github.com>
2026-03-18 16:43:51 -0400

    Handle nightly updater fallback

--- src/app/updater/updater.component.ts
+++ src/app/updater/updater.component.ts
@@ -11,9 +11,11 @@ import { UpdateProgressDialogComponent } from 'app/dialogs/update-progress-dialo
 })
 export class UpdaterComponent implements OnInit {
 
-  availableVersions = null;
+  readonly NIGHTLY_VERSION_LABEL = 'nightly';
+  availableVersions = [];
   availableVersionsFiltered = [];
   versionsShowLimit = 5;
+  versionsLoaded = false;
   latestStableRelease = null;
   selectedVersion = null;
   CURRENT_VERSION = CURRENT_VERSION;
@@ -32,23 +34,53 @@ export class UpdaterComponent implements OnInit {
     });
   }
 
-  getAvailableVersions() {
+  get hasStableVersions(): boolean {
+    return !!this.latestStableRelease && this.availableVersionsFiltered.length > 0;
+  }
+
+  canUpdateSelectedVersion(): boolean {
+    return this.hasStableVersions && !!this.selectedVersion && this.selectedVersion !== CURRENT_VERSION;
+  }
+
+  setNightlyFallback(): void {
+    this.latestStableRelease = null;
     this.availableVersionsFiltered = [];
-    this.postsService.getAvailableRelease().subscribe(res => {
-      this.availableVersions = res;
-      for (let i = 0; i < this.availableVersions.length; i++) {
-        const currentVersion = this.availableVersions[i];
-        // if a stable release has not been found and the version is not "rc" (meaning it's stable) then set it as the stable release
-        if (!this.latestStableRelease && !currentVersion.tag_name.includes('rc')) {
-          this.latestStableRelease = currentVersion;
-          this.selectedVersion = this.latestStableRelease.tag_name;
+    this.selectedVersion = this.NIGHTLY_VERSION_LABEL;
+  }
+
+  getAvailableVersions() {
+    this.versionsLoaded = false;
+    this.latestStableRelease = null;
+    this.selectedVersion = null;
+    this.availableVersionsFiltered = [];
+    this.postsService.getAvailableRelease().subscribe({
+      next: res => {
+        this.availableVersions = Array.isArray(res) ? res : [];
+        for (let i = 0; i < this.availableVersions.length; i++) {
+          const currentVersion = this.availableVersions[i];
+          // if a stable release has not been found and the version is not "rc" (meaning it's stable) then set it as the stable release
+          if (!this.latestStableRelease && !currentVersion.tag_name.includes('rc')) {
+            this.latestStableRelease = currentVersion;
+            this.selectedVersion = this.latestStableRelease.tag_name;
+          }
+
+          if (this.latestStableRelease && i >= this.versionsShowLimit) {
+            break;
+          }
+
+          this.availableVersionsFiltered.push(currentVersion);
         }
 
-        if (this.latestStableRelease && i >= this.versionsShowLimit) {
-          break;
+        if (!this.hasStableVersions) {
+          this.setNightlyFallback();
         }
 
-        this.availableVersionsFiltered.push(currentVersion);
+        this.versionsLoaded = true;
+      },
+      error: () => {
+        this.availableVersions = [];
+        this.setNightlyFallback();
+        this.versionsLoaded = true;
       }
     });
   }
