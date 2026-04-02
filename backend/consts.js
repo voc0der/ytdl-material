74046168efdb11211f7147ad15aa1f5f2004d27d
voc0der <23507129+voc0der@users.noreply.github.com>
2026-04-02 14:24:53 -0400

    chore(release): prepare v1.0.1

--- backend/consts.js
+++ backend/consts.js
@@ -473,4 +473,4 @@ exports.SUBSCRIPTION_BACKUP_PATH = 'subscription_backup.json'
 // we're using a Set here for performance
 exports.YTDL_ARGS_WITH_VALUES = new Set(YTDL_ARGS_WITH_VALUES);
 
-exports.CURRENT_VERSION = 'v1.0.0';
+exports.CURRENT_VERSION = 'v1.0.1';
--- src/app/consts.ts
+++ src/app/consts.ts
@@ -1,1 +1,1 @@
-export const CURRENT_VERSION = 'v1.0.0';
+export const CURRENT_VERSION = 'v1.0.1';
